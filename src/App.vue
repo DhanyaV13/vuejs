d54d8b3759ead0f4f28491478e33f35b74f40386
Dhanya <dhanyavenkatesh13@gmail.com>
2019-07-16 18:41:58 +0200

    App vue changes

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,24 @@
 <template>
    <div id="app">
-    <div class="header">
-      <ul id="ulhead">
-          <li><a class="active" href="/">Home</a></li>
-          <li><a href="#">Categories</a></li>
-          <li><a href="/random">Random</a></li>
-      </ul>
-    </div>
+     <nav-bar></nav-bar>
     <router-view/>
-    <div class="footer">
-      <ul class="footul">
-        <li align="left">
-          <a href="/about" style="color:white;"> About </a>
-        </li>
-        <li style="float:right">
-          <a href="/search" style="color:white">Search</a>
-        </li>
-      </ul>
-      <div align="center">&copy; {{ new Date().getFullYear() }}</div>
-    </div>
+    <bottom-bar></bottom-bar>
   </div>
 </template>
 
 
 <script lang="ts">
 import Vue from 'vue'
+import Component from 'vue-class-component'
+import Header from '@/components/TheHeader.vue'
+import Footer from '@/components/TheFooter.vue'
 
+@Component({
+  components:{
+    'nav-bar': Header,
+    'bottom-bar': Footer
+    }
+})
 export default class App extends Vue {
   title = "Vue Books";
   
@@ -35,48 +28,19 @@ export default class App extends Vue {
 <style>
 #app {
   font-family: 'Ubuntu', sans-serif;
-  height: 100%;
+  height: auto;
+  width : auto;
 }
 
-.header{
-  position: fixed;
-  width: 100%;
-}
 
-.branding {
-  flex: 1;
-  text-align: left;
-}
- 
-#ulhead{
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color:green;
-}
-
-#ulhead li {
-  float: left;
-}
-
-#ulhead li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-#ulhead li a:hover {
-  background-color:lightgreen;
-}
 .footer{
   margin:auto;
   width:100%;
   height:auto;
   overflow:hidden;
   background-color: green;
+  position: fixed;
+  bottom: 0;
 }
 
 h1 {
@@ -87,10 +51,6 @@ h1 {
   display: inline-block;
    margin-left:80px; 
 }
-
-/* .footul li:first-child {
-    margin-left:0px;
-} */
 
 .footul li:nth-child(2){
   margin-right: 100px;
